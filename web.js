9e841a57d11be8af02497cbe950c7875859f40ad
@nelsonic <nodecoder@gmail.com>
2013-04-16 21:32:26 +0100

    new route /test

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/test', function(request, response) {
+  response.send('Hello TEST!');
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
