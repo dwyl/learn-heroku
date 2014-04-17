aa50d904431cb617da63ccecf4cec3e8a175d5ba
Nelson <nodecoder@gmail.com>
2014-04-17 14:32:09 +0100

    Update web.js to only use double quotes.

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get("/", function(request, response) {
+  response.send("Hello World!");
 });
 
-app.get('/test', function(request, response) {
-  response.send('Hello TEST!');
+app.get("/test", function(request, response) {
+  response.send("Hello TEST!");
 });
 
 var port = process.env.PORT || 5000;
