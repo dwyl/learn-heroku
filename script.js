393fc6fa6207cd90605f17a1ab7fb13c0fe33d2d
MyCatt <43209825+MyCatt@users.noreply.github.com>
2019-03-07 21:16:43 +1300

    Update script.js

--- script.js
+++ script.js
@@ -215,6 +215,7 @@ function randomGreeting() {
   }
 
   function updateRow(data, i) {
+    console.log(i);
     data = [data.childNodes[1].value, data.childNodes[2].value, data.childNodes[3].value, data.childNodes[5].value, data.childNodes[0].value]
     var ranges = ("A" + i+1 + ":E" + i+1);
     var values = [
