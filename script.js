e051fce8ded100d2746751c677ceb14b402d1289
Michael Catterall <hey@michaelcatterall.co.nz>
2019-03-09 20:27:19 +1300

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ function randomGreeting() {
           listMajors();
           window.setInterval(function(){
             listMajors();
-          }, 10000);
+          }, 60000);
         } else {
           authorizeButton.style.display = 'block';
           header.style.display = 'block';
